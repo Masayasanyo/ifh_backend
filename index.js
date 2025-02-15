ce412fdfd0200bc126286ae61562cbb40a8ed2fe
Masaya <macbeth0129will@gmail.com>
2025-02-15 21:27:41 +0900

    add profile picture

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const pool = new Pool({
 });
 
 app.post('/signup', async (req, res) => {
-    const { username, email, password, firstName, familyName } = req.body;
+    const { username, email, password, firstName, familyName, imagePath } = req.body;
     if (!username || !email || !password || !firstName || !familyName) {
         return res.status(400).json({ error: "Fill Everything" });
     }
@@ -27,8 +27,8 @@ app.post('/signup', async (req, res) => {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         const result = await pool.query(
-            "INSERT INTO accounts (username, email, password_hash, first_name, family_name) VALUES ($1, $2, $3, $4, $5) RETURNING id, username, email, first_name, family_name, created_at",
-            [username, email, hashedPassword, firstName, familyName]
+            "INSERT INTO accounts (username, email, password_hash, first_name, family_name, profile_image_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, username, email, first_name, family_name, profile_image_url, created_at",
+            [username, email, hashedPassword, firstName, familyName, imagePath]
         );
         res.status(201).json({ message: "Registration Successful.", user: result.rows[0] });
     } catch (error) {
@@ -52,7 +52,7 @@ app.post('/login', async (req, res) => {
         if (!passwordMatch) {
             return res.status(401).json({ error: "Email or Password are wrong." });
         }
-        res.json({ message: "Login Successful", user: { id: user.id, username: user.username, email: user.email, firstName: user.first_name, familyName: user.family_name} });
+        res.json({ message: "Login Successful", user: { id: user.id, username: user.username, email: user.email, firstName: user.first_name, familyName: user.family_name, imagePath: user.profile_image_url} });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Sever Error" });
@@ -109,6 +109,23 @@ app.post("/upload/thumbnail", thumbnailUpload.single("thumbnail"), (req, res) =>
     }
     else {
         return res.status(201).json({ message: 'Success!', filePath: `/thumbnails/${req.file.filename}` }); 
+    }
+});
+
+// Upload profil image
+const profileStorage = multer.diskStorage({
+    destination: "./profile_image",
+    filename: (req, file, cb) => {
+        const newFileName = Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);    },
+});
+const profileUpload = multer({ storage: profileStorage });
+app.post("/upload/profile_image", profileUpload.single("profile_image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: "File not found" });
+    }
+    else {
+        return res.status(201).json({ message: 'Success!', filePath: `/profile_image/${req.file.filename}` }); 
     }
 });
 
@@ -173,6 +190,8 @@ const MOVIE_DIR = path.join(__dirname, '/movies');
 app.use('/movies', express.static(MOVIE_DIR));
 const THUMBNAIL_DIR = path.join(__dirname, '/thumbnails');
 app.use('/thumbnails', express.static(THUMBNAIL_DIR));
+const PROFILE_DIR = path.join(__dirname, '/profile_image');
+app.use('/profile_image', express.static(PROFILE_DIR));
 
 app.get('/films', async (req, res) => {
     try {
@@ -186,6 +205,41 @@ app.get('/films', async (req, res) => {
     }
 });
 
+app.get('/films/latest', async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM films ORDER BY created_at DESC LIMIT 10;"
+        );
+        res.status(200).json({ message: "Fetch Films Successful.", data: result.rows });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+app.get('/films/trending', async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT f.* FROM films f LEFT JOIN likes l ON f.id = l.film_id WHERE f.created_at >= NOW() - INTERVAL '7 days' GROUP BY f.id ORDER BY COUNT(l.id) DESC LIMIT 10;"
+        );
+        res.status(200).json({ message: "Fetch Films Successful.", data: result.rows });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+app.get('/user/featured', async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM accounts ORDER BY created_at DESC LIMIT 10;"
+        );
+        res.status(200).json({ message: "Fetch Filmmakers Successful.", data: result.rows });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
 
 app.post('/accounts', async (req, res) => {
     const { username } = req.body;
@@ -229,6 +283,65 @@ app.post('/members', async (req, res) => {
     }
 
 });
+
+app.post('/films/like/initial', async (req, res) => {
+    const { userId, filmId } = req.body;
+
+    try {
+        const result = await pool.query(
+            "SELECT 1 FROM likes WHERE user_id = $1 AND film_id = $2 LIMIT 1;",
+            [userId, filmId]
+        );
+        const isLiked = result.rowCount > 0;
+
+        res.status(200).json({ isLiked });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+app.post('/films/like', async (req, res) => {
+    const { userId, filmId } = req.body;
+
+    try {
+        await pool.query(
+            "INSERT INTO likes (user_id, film_id) VALUES ($1, $2) ON CONFLICT (user_id, film_id) DO NOTHING;",
+            [userId, filmId]
+        );
+        res.status(200).json({ message: "Liked successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+app.post('/films/dislike', async (req, res) => {
+    const { userId, filmId } = req.body;
+
+    try {
+        await pool.query(
+            "DELETE FROM likes WHERE user_id = $1 AND film_id = $2;",
+            [userId, filmId]
+        );
+        res.status(200).json({ message: "Disliked successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+// app.post('/films/like/count', async (req, res) => {
+//     const { filmId } = req.params;
+
+//     try {
+//         const result = await pool.query("SELECT COUNT(*) FROM likes WHERE film_id = $1;", [filmId]);
+//         res.status(200).json({ likes: result.rows[0].count });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: "Server Error" });
+//     }
+// });
 
 
 
